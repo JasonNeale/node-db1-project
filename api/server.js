787e68f47c4285679d7879f2955890020842c871
Jason Neale <jg13.1980@gmail.com>
2020-07-22 19:54:15 -0400

    Completed MVP

--- api/server.js
+++ api/server.js
@@ -1,9 +1,24 @@
-const express = require("express");
+require('dotenv').config()
 
-const db = require("../data/dbConfig.js");
+const { logger, lockout, handleErrors } = require('../middleware/server-mw')
+const express = require('express')
+const cors = require('cors')
+const DbRouter = require('../router/dbRouter')
+const helmet = require('helmet')
+const server = express()
+// const port = process.env.PORT || 5000
 
-const server = express();
+// server.listen(port)
+server.use(express.json())
+server.use('/api/accounts', DbRouter)
+server.use(cors())
+server.use(helmet())
+server.use(logger)
 
-server.use(express.json());
+server.get('/', (req, res) => {
+    res.send(`<span style="text-align:center; margin-top:100px;"><h3>Welcome to</h3></h3><h1>Some Cool Company Name\'s</h1><h2>API Server</h2></span>`)
+})
 
-module.exports = server;
+server.use(handleErrors)
+
+module.exports = server
